c52b0d9d9fc85b8de36b533a2788bd96c66c0a24
basechase <chase7y@gmail.com>
2026-08-02 15:33:18 -0500

    raycasting from cam to worldspace

--- Source/MagicProject/MyGameplayAbility.cpp
+++ Source/MagicProject/MyGameplayAbility.cpp
@@ -4,6 +4,7 @@
 #include "MyGameplayAbility.h"
 
 #include "DrawDebugHelpers.h"
+#include "Camera/CameraActor.h"
 #include "Engine/World.h"
 #include "GameFramework/Character.h"
 #include "ViewportToolbar/UnrealEdViewportToolbar.h"
@@ -22,14 +23,38 @@ void UMyGameplayAbility::ActivateAbility(const FGameplayAbilitySpecHandle Handle
 	float Pitch = PC->PlayerCameraManager->GetCameraRotation().Pitch;
 	ACharacter* Character = Cast<ACharacter>(Info->AvatarActor.Get());
 	
+	FVector StartingCamLocation = PC->PlayerCameraManager->GetCameraLocation();
+	FVector EndLocation = StartingCamLocation + CameraRotation.Vector() * 100000.f;
+	
+	FCollisionQueryParams CollisionParams;
+	CollisionParams.AddIgnoredActor(Character);
+	FHitResult Hit;
+	
+	
+	
 	Super::ActivateAbility(Handle, Info, ActivationInfo, TriggerEventData);
 	UE_LOG(LogTemp, Warning, TEXT("FIRE ABILITY ACTIVATED"));
 	
 	if (Character)
 	{
-		DrawDebugSphere(Character->GetWorld(), Character->GetActorLocation(), 16, 16, FColor::Green, false, 3.0f);
 		
-		GetWorld()->SpawnActor<AActor>(BulletActor, Character->GetActorLocation()+ Character->GetActorForwardVector()* 200, FRotator (Pitch, Character->GetActorRotation().Yaw,0.f));
+		GetWorld()->LineTraceSingleByChannel(
+			Hit,
+			StartingCamLocation,
+			EndLocation,
+			ECC_Visibility,
+			CollisionParams);
+		
+		DrawDebugLine(
+	GetWorld(),
+	StartingCamLocation,
+	EndLocation,
+	FColor::Red,
+	false,
+	1.0f,
+	0,
+	2.0f);
+		
 	}
 	
 	
